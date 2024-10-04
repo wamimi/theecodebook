e89f38bd992a93c70a6f4909eb060f2e73921730
wamimi <nellycyberpro@gmail.com>
2024-10-04 19:59:06 +0300

    general sub cipher implementation

---
+++ src/generalsubstitutionalgo/general.rs
@@ -0,0 +1,10 @@
+// importing the necessary modules from the std library and also external crates
+use std::collections::HashMap; //Used for storing the substitution key
+use rand::seq::SliceRandom; //for shuflling the alphabet
+use rand ::thread_rng; //Also for generating random numbers
+use rand::Rng; //for generating random numbers
+// function to generate a random substitution key
+fn generate_key() -> HashMap< char, char> {
+
+    // We need to create a vector
+}
